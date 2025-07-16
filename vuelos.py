9249c8010f4d16a9d5d2edc7a4b84b99b737ec3a
AndreaMartinezC <andrea12_94@hotmail.com>
2025-07-15 19:17:17 -0600

    Agregando archivo encargado de usar serpapi para consultar vuelos

---
+++ vuelos.py
@@ -0,0 +1,40 @@
+from serpapi import GoogleSearch
+import json
+
+def setparameters(departure, arrival, outbound_date, return_date):
+    params = {
+      "engine": "google_flights",
+      "departure_id": departure,
+      "arrival_id": arrival,
+      "outbound_date": outbound_date,
+      "return_date": return_date,
+      "currency": "MXN",
+      "hl": "en",
+      "api_key": "9d7d027cff59c9d81a06238f00abeb0574471f5da88e2ad8b7b5021c7002d8af"
+    }
+
+    return params
+
+vuelos = []
+
+params = setparameters("ELP", "LAX", "2025-07-17","2025-07-22" )
+search = GoogleSearch(params)
+results = search.get_dict()['best_flights']
+
+with open("output.json", "w") as file:
+  json.dump(results, file, indent=4)# Code to write JSON data will go here
+
+for item in results:
+    flights = item.get('flights')
+    departureairport = flights[0]['departure_airport']['name']
+    departuretime = flights[0]['departure_airport']['time']
+    arrivalairport = flights[0]['arrival_airport']['name']
+    arrivaltime = flights[0]['arrival_airport']['time']
+    airplane = flights[0]['airplane']
+    airline = flights[0]['airline']
+    flightnumber = flights[0]['flight_number']
+    totalduration = item.get('total_duration')
+    price = item.get('price')
+    print(departureairport + " " + departuretime)
+    print(arrivalairport + " " + arrivaltime)
+    print(airplane + " " + airline + " " + flightnumber + " " + str(totalduration) + " " + str(price))
