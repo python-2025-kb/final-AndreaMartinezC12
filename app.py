8f1c97ae735a7a3b864ed337343a348dc9b0f1a4
AndreaMartinezC <andrea12_94@hotmail.com>
2025-07-17 19:52:26 -0600

    Agregando template de formulario y resultados. El formulario ya funciona con lo basico para hoteles, falta acomodar bien la informacion en resultados

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
+import hoteles
 
 app=Flask(__name__)
 
+resultadohoteles=[]
+
 @app.route('/')
 def home():
-    return render_template('index.html', nombre='Andrea')
+    return render_template('inicio.html')
+
+@app.route('/formulario', methods=['GET','POST'])
+def formulario():
+    global resultadohoteles
+    if request.method == 'POST':
+        destino = request.form['destino']
+        aeropuertoorigen = request.form['aeropuertoorigen']
+        aeropuertodestino = request.form['aeropuertodestino']
+        fechasalida = request.form['fechasalida']
+        fecharegreso = request.form['fecharegreso']
+        personas = request.form['personas']
+        presupuesto = request.form['presupuesto']
+        resultadohoteles = hoteles.searchhotels(destino, fechasalida, fecharegreso, personas)
+        print(resultadohoteles)
+        #contador_id += 1
+        return redirect(url_for('resultados'))
+    return render_template('formulario.html')
+
+@app.route('/resultados')
+def resultados():
+    return render_template('resultados.html', opcioneshoteles = resultadohoteles)
 
 if __name__ == '__main__':
     app.run(debug=True)
